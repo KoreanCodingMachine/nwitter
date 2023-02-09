894f88628c5d743cc584eac6dcc9e51113683571
KoreanCodingMachine <pon03078@naver.com>
2023-02-10 03:51:25 +0900

    CRUD 구현중

--- src/MyBase.js
+++ src/MyBase.js
@@ -1,17 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_API_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: 'AIzaSyAT390DVY7orRo2z-KreMXCDRXYVcbEkd0',
+  authDomain: 'nwitter-9d7d7.firebaseapp.com',
+  projectId: 'nwitter-9d7d7',
+  storageBucket: 'nwitter-9d7d7.appspot.com',
+  messagingSenderId: '987255372441',
+  appId: '1:987255372441:web:0b9055f9569c13fcc11608',
+  measurementId: 'G-GECWK7X84G',
 };
 
 export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,74 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { dbService } from 'MyBase';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const q = query(collection(dbService, 'nweets'));
+    console.log(q);
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      console.log(nweetObj);
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, 'nweets'), {
+        nweet,
+        createdAt: Date.now(),
+      });
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+
+    setNweet('');
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder='whats on your mind'
+          maxLength={120}
+          onChange={onChange}
+          value={nweet}
+        />
+        <input type='submit' name='Nweet' />
+      </form>
+      <div>
+        {nweets.length === 0
+          ? 'loading...'
+          : nweets.map((nweet) => (
+              <div key={nweet.id}>
+                <h4>{nweet.nweet}</h4>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
